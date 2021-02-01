15f8ef91b69ad23c980e7f8d170cbb0cae64c566
Zayd Carelse <36879135+ZaydCarelse@users.noreply.github.com>
2021-02-01 22:12:09 +0200

    Setting up development environment with test

---
+++ Project 1/main.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+
+int main()
+{
+    int favNumber;
+    std::cout << "Enter your favourite number between 1 and 100: "; 
+    std::cin >> favNumber;
+    std::cout << "Amazing! That's my favourite number too!" << std::endl;
+    std::cout << "No really, " << favNumber << " is my favourite number too!" << std::endl;
+    return 0;
+}
