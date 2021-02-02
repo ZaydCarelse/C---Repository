91f55fe0a9f1c042c3c9c52309e94489b6d721be
Zayd Carelse <36879135+ZaydCarelse@users.noreply.github.com>
2021-02-02 22:31:27 +0200

    Update to include different sized rooms
    Added price per small room and large room and then created variables to hold frequently used equations.

--- Project 3/CustomerReceipt.cpp
+++ Project 3/CustomerReceipt.cpp
@@ -7,20 +7,30 @@ int main()
     cout << "Welcome to Ray's Cleaning services!" << endl;
     cout << "================================================" << endl;
     cout << "We aim to clean your home as if it were our own!" << endl;
-    cout << "\nHow many rooms do you have to clean? ";
+    
+    cout << "\nHow many large rooms do you have to clean? ";
+    int l_rooms_to_clean {0};
+    cin >> l_rooms_to_clean;
+    
+    cout << "\nHow many small rooms do you have to clean? ";
+    int s_rooms_to_clean {0};
+    cin >> s_rooms_to_clean;
 
-    int rooms_to_clean {0};
-    cin >> rooms_to_clean;
+    const double s_price_per_room {25};
+    const double l_price_per_room {40};
+    cout << "\nPrice per large room: $" << l_price_per_room << endl;
+    cout << "Price per small room: $" << s_price_per_room << endl;
 
-    const double price_per_room {25};
-    cout << "\nPrice per room: $" << price_per_room << endl;
+    double total_cost = (s_price_per_room * s_rooms_to_clean) + (l_price_per_room * l_rooms_to_clean);
 
     const double tax_rate {0.06};
+    double cost_with_tax = (total_cost * tax_rate);
+
     int validity {30};
-    cout << "Total Cost: $" << price_per_room * rooms_to_clean << endl;
-    cout << "Tax: $" << price_per_room * rooms_to_clean * tax_rate << endl;
+    cout << "Total Cost: $" << total_cost << endl;
+    cout << "Tax: $" << total_cost * tax_rate << endl;
     cout << "================================================" << endl;
-    cout << "Total Estimate: $" << (price_per_room * rooms_to_clean * tax_rate) + (price_per_room * rooms_to_clean) << endl;
+    cout << "Total Estimate: $" << total_cost + (total_cost * tax_rate) << endl;
     cout << "This estimate is valid for " << validity << " days." << endl;
 
     return 0;
